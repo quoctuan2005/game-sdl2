fbceb8afe28e40def3e8c08c5b5770128999e1c5
quoctuan2005 <nqtuan05@gmail.com>
2024-05-23 08:24:18 +0700

    Initial Commit

---
+++ game-sdl2/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  game-sdl2
+//
+//  Created by Nguyen Quoc Tuan on 23/5/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ game-sdl2/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  game-sdl2
+//
+//  Created by Nguyen Quoc Tuan on 23/5/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
